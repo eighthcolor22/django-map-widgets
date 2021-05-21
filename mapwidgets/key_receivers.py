57413081db72ffc723024d38c1ecf9b7439c4a87
eugene <eighthcolor22@gmail.com>
2021-05-21 17:05:29 +0300

    improve get key func

--- mapwidgets/key_receivers.py
+++ mapwidgets/key_receivers.py
@@ -1,19 +1,12 @@
 from django.conf import settings as django_settings
+from mapwidgets.settings import MapWidgetSettings, mw_settings
 
 __all__ = (
     'get_google_maps_api_key',
 )
 
 def get_google_maps_api_key() -> str:
-    key = None
     if 'constance' in django_settings.INSTALLED_APPS:
         from constance import config
-        key = getattr(config, 'GOOGLE_MAPS_API_KEY', None)
+        return getattr(config, 'GOOGLE_MAPS_API_KEY', mw_settings.GOOGLE_MAPS_API_KEY)
 
-    if not key:
-        key = django_settings.GOOGLE_MAPS_API_KEY
-
-    if not key:
-        raise AttributeError("Invalid settings api key")
-
-    return key
